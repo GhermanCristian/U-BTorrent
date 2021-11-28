47df1e4f767c2e81e48cfad4b2be87e909d26c06
GhermanCristian <gherman_cristi10@yahoo.com>
2021-11-28 19:56:15 +0200

    #1 Can now determine the IP and port of the peers

--- trackerConnection.py
+++ trackerConnection.py
@@ -1,8 +1,27 @@
+from typing import List, Tuple
 import requests
 from bencode3 import bdecode
 
 
 class TrackerConnection:
+    def __processPeersPart(self, peersPart: str) -> List[Tuple[int, int]]:
+        peerAddressList: List[Tuple[int, int]] = []
+        currentIndex: int = 7  # skip the "5:peers" part
+        peersSize: int = 0
+
+        while peersPart[currentIndex].isdigit():
+            peersSize = peersSize * 10 + int(peersPart[currentIndex])
+            currentIndex += 1
+        currentIndex += 1  # skip the ":"
+        assert peersSize % 6 == 0
+        for _ in range(0, peersSize // 6):
+            currentIP = ord(peersPart[currentIndex]) * 256**3 + ord(peersPart[currentIndex + 1]) * 256**2 + ord(peersPart[currentIndex + 2]) * 256 + ord(peersPart[currentIndex + 3])
+            currentPort = ord(peersPart[currentIndex + 4]) * 256 + ord(peersPart[currentIndex + 5])
+            peerAddressList.append((currentIP, currentPort))  # TODO - create a Peer class
+            currentIndex += 6
+
+        return peerAddressList
+
     def __processResponse(self, response: str) -> [str, dict]:
         peersPosition: int = response.find("5:peers")
         peersPart: str = response[peersPosition: -1]
@@ -34,6 +53,7 @@ class TrackerConnection:
 
         print(peersPart)
         print(nonPeersPart)
+        print(self.__processPeersPart(peersPart))
 
 
 
