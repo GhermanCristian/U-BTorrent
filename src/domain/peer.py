a9ef37fce35e1237e720eed136f2f584a07669e9
GhermanCristian <gherman_cristi10@yahoo.com>
2022-03-17 18:49:16 +0200

    #10 Modified the Peer class to incorporate fields related to choking and interest

--- src/domain/peer.py
+++ src/domain/peer.py
@@ -2,6 +2,10 @@ class Peer:
     def __init__(self, IP: int, port: int):
         self.__IP: int = IP
         self.__port: int = port
+        self.__amChokingIt: bool = True
+        self.__isChokingMe: bool = True
+        self.__amInterestedInIt: bool = False
+        self.__isInterestedInMe: bool = False
 
     @property
     def IP(self) -> int:
@@ -11,12 +15,32 @@ class Peer:
     def port(self) -> int:
         return self.__port
 
+    @property
+    def amChokingIt(self) -> bool:
+        return self.__amChokingIt
+
+    @property
+    def isChokingMe(self) -> bool:
+        return self.__isChokingMe
+
+    @property
+    def amInterestedInIt(self) -> bool:
+        return self.__amInterestedInIt
+
+    @property
+    def isInterestedInMe(self) -> bool:
+        return self.__isInterestedInMe
+
     def __str__(self) -> str:
         firstOctet = (self.__IP // 256 ** 3) % 256
         secondOctet = (self.__IP // 256 ** 2) % 256
         thirdOctet = (self.__IP // 256 ** 1) % 256
         fourthOctet = self.__IP % 256
-        return f'{firstOctet}.{secondOctet}.{thirdOctet}.{fourthOctet}:{self.__port}'
+        return f"""{firstOctet}.{secondOctet}.{thirdOctet}.{fourthOctet}:{self.__port};
+            amChokingIt={self.__amChokingIt}; isChokingMe={self.__isChokingMe};
+            amInterestedInIt={self.__amInterestedInIt}; isInterestedInMe={self.__isInterestedInMe};"""
 
     def __eq__(self, other):
-        return isinstance(other, Peer) and self.__IP == other.IP and self.__port == other.port
+        return isinstance(other, Peer) and self.__IP == other.IP and self.__port == other.port and self.__amChokingIt == other.amChokingIt \
+               and self.__isChokingMe == other.isChokingMe and self.__amInterestedInIt == other.amInterestedInIt \
+               and self.__isInterestedInMe == other.isInterestedInMe
