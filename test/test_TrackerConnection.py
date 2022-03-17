5829f21e6f9e207aaf8b5fb111905b747a103732
GhermanCristian <gherman_cristi10@yahoo.com>
2022-03-17 13:09:58 +0200

    Updated test_TrackerConnection to comply with the modified trackerConnection

--- test/test_TrackerConnection.py
+++ test/test_TrackerConnection.py
@@ -8,15 +8,16 @@ class TestTrackerConnection(unittest.TestCase):
         self.__trackerConnection = TrackerConnection()
 
     def test_computePeers_ValidPeersPart_CorrectPeerList(self) -> None:
-        self.assertEqual(self.__trackerConnection.computePeers("5:peers6:¼xá"), [Peer(3155919480, 6881)])
+        peersPartAsByteArray: bytearray = bytearray(b"5:peers6:\xbc\x1b\x84\x08\x1a\xe1")
+        self.assertEqual(self.__trackerConnection.computePeers(peersPartAsByteArray), [Peer(3155919880, 6881)])
 
     def test_onSuccessfulConnection_ValidContent_CorrectNonPeerContent(self) -> None:
-        nonPeersPart, _ = self.__trackerConnection.onSuccessfulConnection("d8:completei0e10:downloadedi0e10:incompletei1e8:intervali1720e12:min intervali860e5:peers6:¼xáe")
-        self.assertEqual(nonPeersPart, {"complete": 0, "downloaded": 0, "incomplete": 1, "interval": 1720, "min interval": 860})
+        nonPeersPart, _ = self.__trackerConnection.onSuccessfulConnection(b'd8:completei0e10:downloadedi0e10:incompletei1e8:intervali1634e12:min intervali817e5:peers6:\xbc\x1b\x84\x08\x1a\xe1e')
+        self.assertEqual(nonPeersPart, {"complete": 0, "downloaded": 0, "incomplete": 1, "interval": 1634, "min interval": 817})
 
     def test_onSuccessfulConnection_ValidContent_CorrectPeerList(self) -> None:
-        _, peerList = self.__trackerConnection.onSuccessfulConnection("d8:completei0e10:downloadedi0e10:incompletei1e8:intervali1720e12:min intervali860e5:peers6:¼xáe")
-        self.assertEqual(peerList, [Peer(3155919480, 6881)])
+        _, peerList = self.__trackerConnection.onSuccessfulConnection(b'd8:completei0e10:downloadedi0e10:incompletei1e8:intervali1634e12:min intervali817e5:peers6:\xbc\x1b\x84\x08\x1a\xe1e')
+        self.assertEqual(peerList, [Peer(3155919880, 6881)])
 
 
 if __name__ == '__main__':
