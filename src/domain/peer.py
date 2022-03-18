2ac22e71264d0302845a49d536a53b32f3d7deb5
GhermanCristian <gherman_cristi10@yahoo.com>
2022-03-18 11:12:16 +0200

    #10 Added getIPRepresentedAsString to Peer (required by the socket API); Modified its __eq__ method such that it no longer considers choking / interested

--- src/domain/peer.py
+++ src/domain/peer.py
@@ -31,16 +31,18 @@ class Peer:
     def isInterestedInMe(self) -> bool:
         return self.__isInterestedInMe
 
-    def __str__(self) -> str:
+    def getIPRepresentedAsString(self) -> str:
         firstOctet = (self.__IP // 256 ** 3) % 256
         secondOctet = (self.__IP // 256 ** 2) % 256
         thirdOctet = (self.__IP // 256 ** 1) % 256
         fourthOctet = self.__IP % 256
-        return f"""{firstOctet}.{secondOctet}.{thirdOctet}.{fourthOctet}:{self.__port};
+        return f"""{firstOctet}.{secondOctet}.{thirdOctet}.{fourthOctet}"""
+
+    def __str__(self) -> str:
+        return self.getIPRepresentedAsString() + f""":{self.__port};
             amChokingIt={self.__amChokingIt}; isChokingMe={self.__isChokingMe};
             amInterestedInIt={self.__amInterestedInIt}; isInterestedInMe={self.__isInterestedInMe};"""
 
     def __eq__(self, other):
-        return isinstance(other, Peer) and self.__IP == other.IP and self.__port == other.port and self.__amChokingIt == other.amChokingIt \
-               and self.__isChokingMe == other.isChokingMe and self.__amInterestedInIt == other.amInterestedInIt \
-               and self.__isInterestedInMe == other.isInterestedInMe
+        # no need to check for choking / interest; the IP + port should uniquely determine the peer
+        return isinstance(other, Peer) and self.__IP == other.IP and self.__port == other.port
