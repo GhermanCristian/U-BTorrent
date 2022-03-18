67754def46e7318d39e041375157758542d6b20b
GhermanCristian <gherman_cristi10@yahoo.com>
2022-03-18 12:10:26 +0200

    #10 Added a method which checks if the handshake response is valid

--- src/PeerCommunication.py
+++ src/PeerCommunication.py
@@ -18,23 +18,30 @@ class PeerCommunication:
         self.__peerID: str = peerID
 
     def __createHandshakeMessage(self) -> bytes:
-        return self.CURRENT_PROTOCOL_LENGTH + self.CURRENT_PROTOCOL + self.RESERVED_HANDSHAKE_MESSAGE_BYTES +\
-            self.__infoHash + self.__peerID.encode()
+        return self.CURRENT_PROTOCOL_LENGTH + self.CURRENT_PROTOCOL + self.RESERVED_HANDSHAKE_MESSAGE_BYTES + \
+               self.__infoHash + self.__peerID.encode()
 
-    def __connectToPeer(self, otherPeer: Peer) -> None:
-        print(otherPeer)
-        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __checkValidHandshakeResponse(self, handshakeResponse: bytes) -> bool:
+        # can't use CURRENT_PROTOCOL_LENGTH because it's in byte form (\x13), whereas handshakeResponse[0] is in dec form (19)
+        return len(handshakeResponse) >= 68 and handshakeResponse[0] == len(self.CURRENT_PROTOCOL) and \
+               handshakeResponse[1:20] == self.CURRENT_PROTOCOL and handshakeResponse[28:48] == self.__infoHash
+
+    def __getHandshakeResponseFromPeer(self, otherPeer: Peer) -> bytes:
+        clientSocket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         clientSocket.settimeout(60)
         clientSocket.connect((otherPeer.getIPRepresentedAsString(), otherPeer.port))
         clientSocket.sendall(self.__createHandshakeMessage())
-        buffer = clientSocket.recv(2048)
-        print("response = " + str(buffer))
+        handshakeResponse: bytes = clientSocket.recv(2048)
         clientSocket.close()
+        return handshakeResponse
+
+    def __connectToPeer(self, otherPeer: Peer) -> None:
+        print(otherPeer)
+        handshakeResponse: bytes = self.__getHandshakeResponseFromPeer(otherPeer)
+        print(handshakeResponse)
+        assert self.__checkValidHandshakeResponse(handshakeResponse), "Invalid handshake response. Aborting"
 
     def start(self):
         print(self.__createHandshakeMessage())
         # for now, just a POC - connecting to one peer, no async yet
         self.__connectToPeer(random.choice(self.__peerList))
-        """for peer in self.__peerList:
-            if peer != self.HOST:
-                print(peer)"""
