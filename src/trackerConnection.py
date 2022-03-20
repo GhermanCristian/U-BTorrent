eacae425efa8d47ea370f4a5c4fded953e9a2815
GhermanCristian <gherman_cristi10@yahoo.com>
2022-03-20 21:41:41 +0200

    #10 Added trackerResponseScanner

--- src/trackerConnection.py
+++ src/trackerConnection.py
@@ -1,8 +1,8 @@
-from typing import List, Final, Tuple
+from typing import List, Final
 import requests
-from bencode3 import bdecode
 from requests import Response
 from domain.peer import Peer
+from trackerResponseScanner import TrackerResponseScanner
 
 
 class TrackerConnection:
@@ -10,8 +10,6 @@ class TrackerConnection:
     LAST_AVAILABLE_PORT: Final[int] = 6889
     PEER_ID: Final[str] = "ABCDEFGHIJKLMNOPQRST"
     REQUEST_ATTEMPT_TIMEOUT: Final[int] = 30  # seconds
-    PEERS_PART_HEADER: Final[bytes] = b"5:peers"
-    PEER_SIZE: Final[int] = 6  # bytes
     ATTEMPTS_TO_CONNECT_TO_TRACKER: Final[int] = 3
     SUCCESS_STATUS_CODE: Final[int] = 200
     PAYLOAD_INFO_HASH_KEY: Final[str] = "info_hash"
@@ -23,44 +21,6 @@ class TrackerConnection:
     PAYLOAD_COMPACT_KEY: Final[str] = "compact"
     PAYLOAD_EVENT_KEY: Final[str] = "event"
     PAYLOAD_EVENT_STARTED: Final[str] = "started"
-
-    """
-    Determines the list of peers from the tracker response
-    @:param peersPart - a bytearray containing some headers and the extended ASCII-encoded values representing the IP:port of the peers
-    The header starts with "5:peers" - part of the bencode standard, then a decimal number = the number of bytes needed for the IPs and ports, which is followed by a colon.
-    The decimal number has to be a multiple of 6 (4 bytes for each IP, 2 for each port)
-    @:return a list of Peer objects, extracted from the input
-    """
-    def computePeers(self, peersPart: bytearray) -> List[Peer]:
-        peerAddressList: List[Peer] = []
-        currentIndex: int = len(self.PEERS_PART_HEADER)  # skip the "5:peers" part
-        peersByteCount: int = 0  # the number of bytes used to represent peer addresses
-
-        while 48 + 0 <= peersPart[currentIndex] <= 48 + 9:
-            peersByteCount = peersByteCount * 10 + peersPart[currentIndex] - 48
-            currentIndex += 1
-        currentIndex += 1  # skip the ":"
-        assert peersByteCount % self.PEER_SIZE == 0, f"The number of bytes for the peers IPs and ports should be a multiple of {self.PEER_SIZE}"
-        
-        for _ in range(0, peersByteCount // self.PEER_SIZE):
-            currentIP = peersPart[currentIndex] * 256**3 + peersPart[currentIndex + 1] * 256**2 + peersPart[currentIndex + 2] * 256 + peersPart[currentIndex + 3]
-            currentPort = peersPart[currentIndex + 4] * 256 + peersPart[currentIndex + 5]
-            peerAddressList.append(Peer(currentIP, currentPort))
-            currentIndex += self.PEER_SIZE
-
-        return peerAddressList
-
-    # TODO - separate trackerConnection and the part which parses the tracker response
-    """
-    Processes the tracker response
-    @:param responseBytes - the response to the GET request made to the tracker
-    """
-    def onSuccessfulConnection(self, responseBytes: bytes) -> Tuple[dict, List[Peer]]:
-        responseAsByteArray: bytearray = bytearray(responseBytes)
-        peersPartStartingPosition: int = responseAsByteArray.find(self.PEERS_PART_HEADER)
-        peersPart: bytearray = responseAsByteArray[peersPartStartingPosition: -1]  # exclude a trailing 'e'
-        nonPeersPart: dict = bdecode(responseAsByteArray.replace(peersPart, b""))
-        return nonPeersPart, self.computePeers(peersPart)
 
     """
     Computes the peer list according to the torrent meta info file
@@ -87,7 +47,7 @@ class TrackerConnection:
                 try:
                     response: Response = requests.get(announceURL, params=payload, timeout=self.REQUEST_ATTEMPT_TIMEOUT)
                     if response.status_code == self.SUCCESS_STATUS_CODE:
-                        nonPeersPart, peerList = self.onSuccessfulConnection(response.content)
+                        nonPeersPart, peerList = TrackerResponseScanner.scanTrackerResponse(response.content)
                         # nonPeersPart is not used right now, but it probably will be in the future
                         return peerList
                 except Exception as e:
