e806a4f53843ae3333c68c4adcd05324bd694832
GhermanCristian <gherman_cristi10@yahoo.com>
2022-03-21 11:45:46 +0200

    #12 Can now store the active peer connections

--- src/main.py
+++ src/main.py
@@ -1,31 +1,14 @@
 from typing import List
 from domain.peer import Peer
+from peerCommunication import PeerCommunication
 from torrentMetaInfoScanner import TorrentMetaInfoScanner
 from trackerConnection import TrackerConnection
 
 
-def printScannerInformation(scanner: TorrentMetaInfoScanner) -> None:
-    print(scanner.getAnnounceURL())
-    print(scanner.getAnnounceURLList())
-    print(scanner.getTorrentName())
-    print(scanner.getPieceLength())
-    print(scanner.getPieces())
-    for file in scanner.getFiles():
-        print(file)
-    print(scanner.getInfoHash())
-    print(scanner.getTotalContentSize())
-
-
-def printPeerList(scanner: TorrentMetaInfoScanner) -> None:
-    peerList: List[Peer] = TrackerConnection().getPeerList(scanner.getAnnounceURL(), scanner.getInfoHash(), scanner.getTotalContentSize())
-    for peer in peerList:
-        print(peer)
-
-
 def main():
-    scanner: TorrentMetaInfoScanner = TorrentMetaInfoScanner("Resources/NBALOGO_archive.torrent")
-    printScannerInformation(scanner)
-    printPeerList(scanner)
+    scanner: TorrentMetaInfoScanner = TorrentMetaInfoScanner("Resources/Tails 4.28 64Bit ISO.torrent")
+    peerList: List[Peer] = TrackerConnection().getPeerList(scanner.getAnnounceURL(), scanner.getInfoHash(), scanner.getTotalContentSize())
+    PeerCommunication(peerList, scanner.getInfoHash(), TrackerConnection.PEER_ID).start()
 
 
 if __name__ == "__main__":
--- src/peerCommunication.py
+++ src/peerCommunication.py
@@ -1,5 +1,5 @@
 from asyncio import Task, StreamReader, StreamWriter
-from typing import List, Final
+from typing import List, Final, Dict, Tuple
 from domain.peer import Peer
 from domain.validator.handshakeResponseValidator import HandshakeResponseValidator
 import asyncio
@@ -16,10 +16,11 @@ class PeerCommunication:
     MAX_HANDSHAKE_RESPONSE_SIZE: Final[int] = 1024
 
     def __init__(self, peerList: List[Peer], infoHash: bytes, peerID: str):
-        self.__peerList: List[Peer] = peerList
+        self.__initialPeerList: List[Peer] = peerList
         self.__infoHash: bytes = infoHash
         self.__peerID: str = peerID
         self.__handshakeResponseValidator: HandshakeResponseValidator = HandshakeResponseValidator(self.__infoHash, self.CURRENT_PROTOCOL)
+        self.__activeConnections: Dict[Peer, Tuple[StreamReader, StreamWriter]] = {}
 
     """
     Creates a handshake message, according to the current protocol and infoHash
@@ -29,18 +30,9 @@ class PeerCommunication:
         return self.CURRENT_PROTOCOL_LENGTH + self.CURRENT_PROTOCOL + self.RESERVED_HANDSHAKE_MESSAGE_BYTES + \
                self.__infoHash + self.__peerID.encode()
 
-    """
-    Sends the handshake to another peer (through a StreamWriter), and receives back a message from it (through a StreamReader)
-    @:return The response from the other peer
-    """
-    async def __exchangeHandshake(self, reader: StreamReader, writer: StreamWriter) -> bytes:
-        writer.write(self.__createHandshakeMessage())
-        await writer.drain()
-        handshakeResponse: bytes = await reader.read(self.MAX_HANDSHAKE_RESPONSE_SIZE)
-        writer.close()
-        await writer.wait_closed()
-
-        return handshakeResponse
+    async def __closeConnection(self, readerWriterPair: Tuple[StreamReader, StreamWriter]) -> None:
+        readerWriterPair[1].close()
+        await readerWriterPair[1].wait_closed()
 
     """
     Tries to connect to another peer in order to exchange handshake messages
@@ -48,26 +40,37 @@ class PeerCommunication:
     """
     async def __attemptToConnectToPeer(self, otherPeer: Peer) -> None:
         for attempt in range(self.ATTEMPTS_TO_CONNECT_TO_PEER):
+            reader, writer = None, None  # cannot really specify types here; the default StreamWriter constructor requires some values
             try:
                 reader, writer = await asyncio.open_connection(otherPeer.getIPRepresentedAsString(), otherPeer.port)
-                # TODO - store reader + writer from valid connections (whose handshake is valid) in a dict
-                handshakeResponse: bytes = await self.__exchangeHandshake(reader, writer)
+                writer.write(self.__createHandshakeMessage())
+                await writer.drain()
+                handshakeResponse: bytes = await reader.read(self.MAX_HANDSHAKE_RESPONSE_SIZE)
                 if self.__handshakeResponseValidator.validateHandshakeResponse(handshakeResponse):
-                    print(str(otherPeer) + " - OK")
+                    self.__activeConnections[otherPeer] = (reader, writer)
+                    return
                 else:
-                    print(str(otherPeer) + " - Invalid handshake response. Aborting") 
+                    await self.__closeConnection((reader, writer))
             except:
-                pass
+                if reader is not None and writer is not None:
+                    await self.__closeConnection((reader, writer))
+
+    async def __closeAllConnections(self):
+        closeConnectionTasks: List[Task] = []
+        for readerWriterPair in self.__activeConnections.values():
+            closeConnectionTasks.append(asyncio.create_task(self.__closeConnection(readerWriterPair)))
+        await asyncio.gather(*closeConnectionTasks)
 
     """
     Attempts to connect to all available peers and exchange handshake messages with them
     """
     async def __connectToPeers(self) -> None:
         connectTasks: List[Task] = []
-        for peer in self.__peerList:
+        for peer in self.__initialPeerList:
             if peer != self.HOST:
                 connectTasks.append(asyncio.create_task(self.__attemptToConnectToPeer(peer)))
         await asyncio.gather(*connectTasks)
+        await self.__closeAllConnections()
 
     """
     Wrapper for the method which connects to the peers
