bbcb019a6ce989ade7e72b30d620b3a1f903afee
GhermanCristian <gherman_cristi10@yahoo.com>
2022-03-22 17:51:46 +0200

    #12 Updated KeepAliveMessage to extend Message

--- src/domain/message/keepAliveMessage.py
+++ src/domain/message/keepAliveMessage.py
@@ -1,12 +1,14 @@
-class KeepAliveMessage:
-    def __init__(self):
-        self.__content: bytes = b""
+from typing import Final
+import utils
+from domain.message.message import Message
 
-    def getMessage(self) -> bytes:
+
+class KeepAliveMessage(Message):
+    LENGTH_PREFIX: Final[int] = 0
+
+    def __init__(self):
+        self.__content: bytes = utils.convertIntegerTo4ByteBigEndian(self.LENGTH_PREFIX)
+
+    def getMessageContent(self) -> bytes:
         return self.__content
 
-    def __eq__(self, otherMessage):
-        return isinstance(otherMessage, KeepAliveMessage) and self.__content == otherMessage.__content
-
-    def __hash__(self):
-        return hash(self.__content)
