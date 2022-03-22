016d58ad5cceb17227b2c1fbc068a7ce8b0917cd
GhermanCristian <gherman_cristi10@yahoo.com>
2022-03-22 17:41:24 +0200

    #12 Updated ChokeMessage and UnchokeMessage to extend MessageWithLengthAndID

--- src/domain/message/unchokeMessage.py
+++ src/domain/message/unchokeMessage.py
@@ -1,17 +1,10 @@
 from typing import Final
+from domain.message.messageWithLengthAndID import MessageWithLengthAndID
 
 
-class UnchokeMessage:
+class UnchokeMessage(MessageWithLengthAndID):
     MESSAGE_ID: Final[int] = 1
+    LENGTH_PREFIX: Final[int] = 1  # messageID = 1B
 
     def __init__(self):
-        self.__content: bytes = chr(self.MESSAGE_ID).encode()
-
-    def getMessage(self) -> bytes:
-        return self.__content
-
-    def __eq__(self, otherMessage):
-        return isinstance(otherMessage, UnchokeMessage) and self.__content == otherMessage.__content
-
-    def __hash__(self):
-        return hash(self.__content)
+        super().__init__(self.LENGTH_PREFIX, self.MESSAGE_ID)
