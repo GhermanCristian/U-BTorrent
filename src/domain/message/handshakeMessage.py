d0f952df38aabb5be235592f9ec7114824bc5b3e
GhermanCristian <gherman_cristi10@yahoo.com>
2022-03-22 17:51:36 +0200

    #12 Updated HandshakeMessage to extend Message

--- src/domain/message/handshakeMessage.py
+++ src/domain/message/handshakeMessage.py
@@ -1,14 +1,16 @@
 from typing import Final
+import utils
+from domain.message.message import Message
 
 
-class HandshakeMessage:
+class HandshakeMessage(Message):
     CURRENT_PROTOCOL: Final[bytes] = b"BitTorrent protocol"
-    CURRENT_PROTOCOL_LENGTH: Final[bytes] = chr(len(CURRENT_PROTOCOL)).encode()  # TODO - make this cleaner
+    CURRENT_PROTOCOL_LENGTH: Final[bytes] = utils.convertIntegerTo1Byte(len(CURRENT_PROTOCOL))
     RESERVED_HANDSHAKE_MESSAGE_BYTES: Final[bytes] = b"00000000"
 
     def __init__(self, infoHash: bytes, peerID: str):
         self.__infoHash: bytes = infoHash
         self.__peerID: bytes = peerID.encode()
 
-    def getMessage(self) -> bytes:
+    def getMessageContent(self) -> bytes:
         return self.CURRENT_PROTOCOL_LENGTH + self.CURRENT_PROTOCOL + self.RESERVED_HANDSHAKE_MESSAGE_BYTES + self.__infoHash + self.__peerID
