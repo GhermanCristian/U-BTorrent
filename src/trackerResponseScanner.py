71ea9d13771d6dae39448c369914310e2c7d4238
GhermanCristian <gherman_cristi10@yahoo.com>
2022-04-15 16:37:47 +0300

    #13 Can now determine the peer list when it is in the dictionary model

--- src/trackerResponseScanner.py
+++ src/trackerResponseScanner.py
@@ -6,6 +6,10 @@ from domain.peer import Peer
 class TrackerResponseScanner:
     PEERS_PART_HEADER: Final[bytes] = b"5:peers"
     PEER_SIZE: Final[int] = 6  # bytes
+    PEERS_DICT_KEY: Final[str] = "peers"
+    PEER_IP_KEY_DICT_MODEL: Final[str] = "ip"
+    PEER_PORT_KEY_DICT_MODEL: Final[str] = "port"
+    DICT_MODEL_IDENTIFIER: Final[bytes] = b"5:peersld2:ip"
 
     """
     Determines the list of peers from the tracker response
@@ -15,8 +19,7 @@ class TrackerResponseScanner:
     @:return a list of Peer objects, extracted from the input
     """
     @staticmethod
-    def computePeers(peersPart: bytearray) -> List[Peer]:
-        # TODO - parse the peers when in dictionary mode
+    def __computePeersBinaryModel(peersPart: bytearray) -> List[Peer]:
         peerAddressList: List[Peer] = []
         currentIndex: int = len(TrackerResponseScanner.PEERS_PART_HEADER)  # skip the "5:peers" part
         peersByteCount: int = 0  # the number of bytes used to represent peer addresses
@@ -35,14 +38,27 @@ class TrackerResponseScanner:
 
         return peerAddressList
 
+    @staticmethod
+    def __scanTrackerResponseBinaryModel(responseBytes: bytes) -> Tuple[dict, List[Peer]]:
+        responseAsByteArray: bytearray = bytearray(responseBytes)
+        peersPartStartingPosition: int = responseAsByteArray.find(TrackerResponseScanner.PEERS_PART_HEADER)
+        peersPart: bytearray = responseAsByteArray[peersPartStartingPosition: -1]  # exclude a trailing 'e'
+        nonPeersPart: dict = bdecode(responseAsByteArray.replace(peersPart, b""))
+        return nonPeersPart, TrackerResponseScanner.__computePeersBinaryModel(peersPart)
+
+    @staticmethod
+    def __scanTrackerResponseDictionaryModel(responseBytes: bytes) -> Tuple[dict, List[Peer]]:
+        decoded: dict = bdecode(responseBytes)
+        peerList: List[Peer] = [Peer(peer[TrackerResponseScanner.PEER_IP_KEY_DICT_MODEL], peer[TrackerResponseScanner.PEER_PORT_KEY_DICT_MODEL]) for peer in decoded[TrackerResponseScanner.PEERS_DICT_KEY]]
+        decoded.pop(TrackerResponseScanner.PEERS_DICT_KEY)
+        return decoded, peerList
+
     """
     Processes the tracker response
     @:param responseBytes - the response to the GET request made to the tracker
     """
     @staticmethod
     def scanTrackerResponse(responseBytes: bytes) -> Tuple[dict, List[Peer]]:
-        responseAsByteArray: bytearray = bytearray(responseBytes)
-        peersPartStartingPosition: int = responseAsByteArray.find(TrackerResponseScanner.PEERS_PART_HEADER)
-        peersPart: bytearray = responseAsByteArray[peersPartStartingPosition: -1]  # exclude a trailing 'e'
-        nonPeersPart: dict = bdecode(responseAsByteArray.replace(peersPart, b""))
-        return nonPeersPart, TrackerResponseScanner.computePeers(peersPart)
+        if responseBytes.find(TrackerResponseScanner.DICT_MODEL_IDENTIFIER) != -1:
+            return TrackerResponseScanner.__scanTrackerResponseDictionaryModel(responseBytes)
+        return TrackerResponseScanner.__scanTrackerResponseBinaryModel(responseBytes)
--- test/test_TrackerResponseScanner.py
+++ test/test_TrackerResponseScanner.py
@@ -4,10 +4,6 @@ from trackerResponseScanner import TrackerResponseScanner
 
 
 class TestTrackerResponseScanner(unittest.TestCase):
-    def test_computePeers_ValidPeersPart_CorrectPeerList(self) -> None:
-        peersPartAsByteArray: bytearray = bytearray(b"5:peers6:\xbc\x1b\x84\x08\x1a\xe1")
-        self.assertEqual(TrackerResponseScanner.computePeers(peersPartAsByteArray), [Peer(3155919880, 6881)])
-
     def test_scanTrackerResponse_ValidContent_CorrectNonPeerContent(self) -> None:
         nonPeersPart, _ = TrackerResponseScanner.scanTrackerResponse(b'd8:completei0e10:downloadedi0e10:incompletei1e8:intervali1634e12:min intervali817e5:peers6:\xbc\x1b\x84\x08\x1a\xe1e')
         self.assertEqual(nonPeersPart, {"complete": 0, "downloaded": 0, "incomplete": 1, "interval": 1634, "min interval": 817})
