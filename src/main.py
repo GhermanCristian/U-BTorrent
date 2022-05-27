a8003086e22e3e9aae6c0457a57b5e1d333f103e
GhermanCristian <gherman_cristi10@yahoo.com>
2022-05-27 17:19:40 +0300

    #17 First version which downloads several torrents in parallel (each download process in a different thread)

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
+from concurrent.futures import ThreadPoolExecutor
 from service.processSingleTorrent import ProcessSingleTorrent
 
 
+def startTorrent(torrentFileLocation: str) -> None:
+    ProcessSingleTorrent(torrentFileLocation)
+
+
 def main():
-    ProcessSingleTorrent("..\\Resources\\thermo.torrent")
+    with ThreadPoolExecutor(3) as executor:
+        executor.submit(startTorrent, "..\\Resources\\medical.torrent")
+        executor.submit(startTorrent, "..\\Resources\\mars.torrent")
+        executor.submit(startTorrent, "..\\Resources\\ppcoin.torrent")
 
 
 if __name__ == "__main__":
