a4ae179c3ddfb41441a45fd9be781a25b373de38
GhermanCristian <gherman_cristi10@yahoo.com>
2022-06-01 10:40:06 +0300

    #19 Refactoring in settingsProcessor.py

--- src/service/settingsProcessor.py
+++ src/service/settingsProcessor.py
@@ -1,25 +1,33 @@
 from configparser import ConfigParser
 from typing import Final
 
-"""This class is basically a singleton"""
+"""This file is basically a singleton"""
 SETTINGS_FILE_PATH: Final[str] = "..\\Resources\\settings.ini"
+DEFAULT_SECTION_NAME: Final[str] = "DEFAULT"
+
+"""have to use snake case instead of camel case, because the config parser is case-insensitive;
+therefore, when it updates the file, it messes-up the key names"""
+INITIAL_DOWNLOAD_LOCATION_KEY: Final[str] = "initial_download_location"
+USER_INTERFACE_REFRESH_RATE_IN_MILLISECONDS: Final[str] = "user_interface_refresh_rate_in_milliseconds"
+
 configParser: ConfigParser = ConfigParser()
 configParser.read(SETTINGS_FILE_PATH)
 
 
 def updateSettingsFile() -> None:
-    with open(SETTINGS_FILE_PATH, "w") as configFile:
+    WRITE_MODE: Final[str] = "w"
+    with open(SETTINGS_FILE_PATH, WRITE_MODE) as configFile:
         configParser.write(configFile)
 
 
 def getDownloadLocation() -> str:
-    return configParser["DEFAULT"]["initial_download_location"]
+    return configParser[DEFAULT_SECTION_NAME][INITIAL_DOWNLOAD_LOCATION_KEY]
 
 
 def setDownloadLocation(newDownloadLocation: str) -> None:
-    configParser.set("DEFAULT", "initial_download_location", newDownloadLocation)
+    configParser.set(DEFAULT_SECTION_NAME, INITIAL_DOWNLOAD_LOCATION_KEY, newDownloadLocation)
     updateSettingsFile()
 
 
 def getUserInterfaceRefreshRate() -> int:
-    return configParser.getint("DEFAULT", "user_interface_refresh_rate_in_milliseconds")
+    return configParser.getint(DEFAULT_SECTION_NAME, USER_INTERFACE_REFRESH_RATE_IN_MILLISECONDS)
