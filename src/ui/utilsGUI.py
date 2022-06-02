b04bd1a8135febf9d5defde90499cc7ef66ac6a7
GhermanCristian <gherman_cristi10@yahoo.com>
2022-06-02 15:48:55 +0300

    #19 Added an image and a (very) basic description in the About U-BTorrent window

--- src/ui/utilsGUI.py
+++ src/ui/utilsGUI.py
@@ -4,3 +4,4 @@ INITIAL_DIRECTORY_PATH: Final[str] = "..\\Resources"
 FONT_NAME: Final[str] = "Calibri"
 CREAM_COLOR: Final[str] = "#f5e6c2"
 NO_MENU_TEAROFF: Final[int] = 0
+LOGO_PATH: Final[str] = "logo.png"
